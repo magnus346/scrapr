d4e86e9b767e22cd8eedccc2c0451119fca85d16
magnus346 <theo@slynt.com>
2023-08-21 19:16:11 +0200

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const axios = require('axios');
+
 exports.handler = async (event) => {
+  const result = await axios.get('https://api.ipify.org/?format=json').catch(err => console.error(err))
   const response = {
     statusCode: 200,
-    body: JSON.stringify("Hello from Lambda and Github!"),
+    body: JSON.stringify("Hello from Lambda and Github : "+JSON.stringify(result.data)),
   }
-  return response
+  return response;
 }
