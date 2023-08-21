428df175716a7dcf0e2c2c4356f0d6d935797ff8
magnus346 <theo@slynt.com>
2023-08-21 19:20:54 +0200

    first commit

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ exports.handler = async (event) => {
     statusCode: 200,
     body: JSON.stringify("Hello from Lambda and Github : "+JSON.stringify(result.data)),
   }
+  // test
   return response;
 }
