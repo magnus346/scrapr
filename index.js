c88785ccae76312a6084384934d548bcbe6acfc6
magnus346 <theo@slynt.com>
2023-11-21 12:22:25 +0100

    'new'

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cheerio = require("cheerio");
 const unirest = require("unirest");
 const AWS = require('aws-sdk');
 
-// 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
+// Mcwf9Ia47w57EDy6DmqZGJEUfjdNl92Lu0ly ghp
 
 const selectRandomUserAgent = () => {
     const userAgents = ["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36"]
