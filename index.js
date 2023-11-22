27bfe99839728a10e74b591724a193fd1b1c89b7
magnus346 <theo@slynt.com>
2023-11-22 09:41:18 +0100

    new

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const AWS = require('aws-sdk');
 // Mcwf9Ia47w57 EDy6DmqZGJEUfjdNl92 Lu0ly ghp
 
 const selectRandomUserAgent = () => {
-    const userAgents = ["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36"]
-    var randomNumber = Math.floor(Math.random() * userAgents.length);
+    const userAgents = ["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36"]
+    const randomNumber = Math.floor(Math.random() * userAgents.length);
     return userAgents[randomNumber];
 }
 
@@ -17,6 +17,7 @@ const scraper = async(keyword, page) => {
 	return unirest
 	.get(url)
 	.headers({
+		"Referer": "https://www.google.com",
 		"User-Agent": selectRandomUserAgent()
 	})
 	.then((response) => {
@@ -28,7 +29,7 @@ const scraper = async(keyword, page) => {
 			throw new Error('Recaptcha');
 		}
 		
-		$("a:has(h3)").each((i,el) => {			
+		$("a[href]:has(h3)").each((i,el) => {			
 			if($(el).attr('href').startsWith('/') === false) {
 				results.push({
 					url: $(el).attr('href'),
@@ -38,9 +39,6 @@ const scraper = async(keyword, page) => {
 		})
 		
 		return results;
-	})
-	.catch((error) => {
-		throw new Error('Recaptcha');
 	});
 }
 
